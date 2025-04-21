16ff3fcc470a4518c8a9ac39e239448c54bcf3b7
Webixx-Store <contact.webixx@gmail.com>
2025-04-21 08:58:46 +0700

    A

---
+++ read_mt5.py
@@ -0,0 +1,13 @@
+import MetaTrader5 as mt5
+if not mt5.initialize():
+    print("MT5 initialization failed:", mt5.last_error())
+    quit()
+account_info = mt5.account_info()
+if account_info is None:
+    print("Không lấy được thông tin tài khoản:", mt5.last_error())
+else:
+    for field in dir(account_info):
+        if not field.startswith("_"):
+            print(f"{field}: {getattr(account_info, field)}")
+
+mt5.shutdown()
